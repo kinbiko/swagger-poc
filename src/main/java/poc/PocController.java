62dc675dd757ff3e0c2aa559eff5a7f4447081f9
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-16 11:20:03 +0000

    Add swagger2 from springfox

--- src/main/java/poc/PocController.java
+++ src/main/java/poc/PocController.java
@@ -1,12 +1,12 @@
 package poc;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PocController {
 
-    @RequestMapping("/poc")
+    @GetMapping("/poc")
     public PocPojo getPoc() {
         return new PocPojo();
     }
